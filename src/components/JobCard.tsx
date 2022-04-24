70287be881ae05c83fed0d5058be1752ea08982a
Jake S <70650432+catalyst4@users.noreply.github.com>
2022-04-24 02:13:13 +0100

    size and shadow changes

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -7,21 +7,21 @@ const JobCard = ({ job }) => {
     <div
       role='button'
       onClick={() => router.push(`/jobs/${job.id}`)}
-      className='shadow-sm bg-white px-3 py-4 rounded-lg mb-3 last:mb-0'
+      className='shadow-md bg-white px-3 py-4 rounded-lg mb-3 last:mb-0'
     >
       <div className='flex items-center'>
         <div className='w-16 h-16 bg-gray-200 mr-4 rounded-lg'></div>
         <div>
-          <div className='flex items-center text-gray-500 text-xs -mb-1'>
+          <div className='flex items-center text-gray-500 text-sm -mb-1'>
             <div className='mr-2'>{job.employer}</div>
             &bull;
             <div className='ml-2'>{job.location}</div>
           </div>
-          <div className='text-lg -mb-1'>{job.title}</div>
-          <div className='flex items-center text-gray-500'>
-            <div className='text-xs uppercase mr-2'>{job.hourlyRate}</div>
+          <div className='text-xl -mb-1'>{job.title}</div>
+          <div className='flex items-center text-gray-500 text-sm'>
+            <div className='uppercase mr-2'>{job.hourlyRate}</div>
             &bull;
-            <div className='text-xs uppercase ml-2'>{job.estHours} hrs</div>
+            <div className='uppercase ml-2'>{job.estHours} hrs</div>
           </div>
         </div>
       </div>
